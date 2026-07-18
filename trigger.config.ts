bbe26b1f793f1d7be2c46986e80c61f5c2c6512b
Adil <shaikhmdadil191@gmail.com>
2026-07-18 17:05:31 +0530

    feat: add right sidebar for workflow editing and update trigger configuration

--- trigger.config.ts
+++ trigger.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "@trigger.dev/sdk"
+import { defineConfig } from "@trigger.dev/sdk";
 
 export default defineConfig({
-  project: "proj_obahqirvbjiataxtqydh",
+  project: "proj_ntfnxpyhigpgyqvfkoso",
   runtime: "node",
   logLevel: "log",
   // The max compute seconds a task is allowed to run. If the task run exceeds this duration, it will be stopped.
@@ -18,5 +18,5 @@ export default defineConfig({
       randomize: true,
     },
   },
-  dirs: ["trigger"],
-})
+  dirs: ["features"],
+});
